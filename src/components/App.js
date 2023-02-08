e5c6b9e425ffa38528c2c803b8e8792fb0d30895
Alexandra Cherepania <alexandra.cherepania@gamil.com>
2023-02-08 13:54:20 +0200

    profile

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,19 @@
+import user from '../users.json';
+import { ProfileUser } from './Profile/Profile'
+
+
+
 export const App = () => {
-    return <div>...</div>
+    return (<div>
+        <ProfileUser
+            username={user.username}
+            tag={user.tag}
+            location={user.location}
+            avatar={user.avatar}
+            stats={user.stats}
+        />
+    </div >);
+
 }
 
 
