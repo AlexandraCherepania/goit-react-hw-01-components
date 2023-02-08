f579fcbdbf62d9ffe2e8d441303eacce87956a6c
Alexandra Cherepania <alexandra.cherepania@gamil.com>
2023-02-08 16:03:36 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import user from '../users.json';
+import data from '../components/Statistics/data'
+import friends from "../components/Friends/friends.json"
 import { ProfileUser } from './Profile/Profile'
+import { Statistics } from './Statistics/Statistics'
+import { FriendItem } from './Friends/FriendItem'
 
 
 
 export const App = () => {
-    return (<div>
+    return (<div style={{
+        display: "flex",
+
+        flexDirection: "column",
+        gap: "15px"
+    }}>
+
         <ProfileUser
             username={user.username}
             tag={user.tag}
@@ -12,6 +22,15 @@ export const App = () => {
             avatar={user.avatar}
             stats={user.stats}
         />
+
+
+        <Statistics title="Upload stats" stats={data} />
+
+        <FriendItem friends={friends} />
+
+
+
+
     </div >);
 
 }
